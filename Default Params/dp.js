700b982bbd15d316580978912e0920b71461570d
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-02-21 20:58:19 -0600

    Default params

---
+++ Default Params/dp.js
@@ -0,0 +1,16 @@
+// function rollDie(numSides){
+//     if (numSides === undefined){
+//         numSides = 6;
+//     }
+//     return Math.floor(Math.random()* numSides) + 1;
+// }
+
+function rollDie(numSides = 6) {
+    return Math.floor(Math.random() * numSides) + 1;
+}
+
+function greet(person, msg = "Hey there") {
+    console.log(`${msg}, ${person}!`);
+}
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Newer JavaScript Features 
+-Default Params
+    *You can add default parameters, just dont make them the first parameter 
+-Spread in Function Calls
+-Spread with Array Literals 
+-Rest Params
+-Destructing Arrays 
+-Destructing Objects 
+-Destructing Params 
